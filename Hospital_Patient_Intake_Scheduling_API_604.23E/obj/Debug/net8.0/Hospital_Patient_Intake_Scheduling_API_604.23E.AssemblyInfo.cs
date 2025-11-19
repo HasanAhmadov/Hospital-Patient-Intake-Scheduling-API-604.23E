c3aa4d9ebf38c4ea514aae395dfe7b28794fae4b
HasanAhmadov <prohesen@gmail.com>
2025-11-19 21:48:47 +0400

    Postgres version updatede

--- Hospital_Patient_Intake_Scheduling_API_604.23E/obj/Debug/net8.0/Hospital_Patient_Intake_Scheduling_API_604.23E.AssemblyInfo.cs
+++ Hospital_Patient_Intake_Scheduling_API_604.23E/obj/Debug/net8.0/Hospital_Patient_Intake_Scheduling_API_604.23E.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Patient_Intake_Scheduling_API_604.23E")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdf44f2d402bb06e640e24ba9ef84885d1519f7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671d77ff9165c865228b04bd613c505bc4a57d40")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Patient_Intake_Scheduling_API_604.23E")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Patient_Intake_Scheduling_API_604.23E")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
