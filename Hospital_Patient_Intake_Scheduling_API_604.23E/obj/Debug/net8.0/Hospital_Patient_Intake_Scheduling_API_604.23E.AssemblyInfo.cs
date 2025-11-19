fdf44f2d402bb06e640e24ba9ef84885d1519f7b
HasanAhmadov <prohesen@gmail.com>
2025-11-19 21:40:21 +0400

    Dockerfile location has been changed

--- Hospital_Patient_Intake_Scheduling_API_604.23E/obj/Debug/net8.0/Hospital_Patient_Intake_Scheduling_API_604.23E.AssemblyInfo.cs
+++ Hospital_Patient_Intake_Scheduling_API_604.23E/obj/Debug/net8.0/Hospital_Patient_Intake_Scheduling_API_604.23E.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Patient_Intake_Scheduling_API_604.23E")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a043b34afbc270515c041bc966e186ddb749eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538224f368170e54e228999e92edda0b0263fe63")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Patient_Intake_Scheduling_API_604.23E")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Patient_Intake_Scheduling_API_604.23E")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
